c0d96a6c01a9701757a9dc50dbbc796f5007dfea
latioms <latioms@gmail.com>
2024-10-30 22:16:53 +0000

    SETUP: initialisation de projet qui e vas pas changer lemode

---
+++ app.py
@@ -0,0 +1,12 @@
+# package des mathematiques pour les fonctions mathematiques
+
+#1 fonction de calcul des discriminants
+
+
+
+#2 fonction de calcul des racines carrees
+
+
+
+#3 multiplication matricielles
+
---
+++ game.py
@@ -0,0 +1,1 @@
+# jeu du juste prix en python
